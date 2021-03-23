88da12b552886246eead3a6d579e46310085ad1b
BreadGarLick <buplasma@gmail.com>
2021-03-23 17:20:59 -0400

    small changes

--- content.js
+++ content.js
@@ -8,7 +8,7 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace(/All You Care To Eat/gi, 'Dining Halls');
+            var replacedText = text.replace(/dies/gi, '');
 
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
